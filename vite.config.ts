bd82927364426551f9cccdc8fd47d31b76aa146a
not_mb <symchychnya@gmail.com>
2024-12-26 15:27:29 +0200

    copy index.html to 404.html on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,17 @@ export default defineConfig({
     sourcemap: true, // Включає генерацію карт джерелnpm
     outDir: "docs", // Вказуємо папку для збірки
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "index.html", // Source file
+          dest: "", // Destination directory in the output folder
+          rename: "404.html", // Rename the file as 404.html
+        },
+      ],
+    }),
+  ],
   base: "/MathTestReact/",
 });
