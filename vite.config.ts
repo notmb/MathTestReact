0dc4c77a415f99ab198e047e9279ee11e8c2929a
not_mb <symchychnya@gmail.com>
2024-12-26 12:06:19 +0200

    br

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true, // Включає генерацію карт джерел
+    sourcemap: true, // Включає генерацію карт джерелnpm
+    outDir: "docs", // Вказуємо папку для збірки
   },
   plugins: [react()],
+  base: "/MathTestReact/",
 });
