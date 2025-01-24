362a172b9daaf2ff0230b14f45ef54cbe6f6b289
not_mb <symchychnya@gmail.com>
2025-01-24 13:55:04 +0200

    updata types

--- src/components/mainContent/tests.tsx
+++ src/components/mainContent/tests.tsx
@@ -3,7 +3,73 @@ import { useEffect, useState } from "react";
 import { db } from "../../firebaseConfig";
 import { getDoc, doc } from "firebase/firestore";
 import TestsItem from "./testItem";
+import TaskOpenAnswer from "./TaskOpenAnswer";
 
+interface Task1 {
+  //тип даних для завдання з з вибором 1 відповіді
+  task: {
+    text: string;
+    table: {
+      value1: string[];
+      velue2: string[];
+    };
+    image: string;
+    list: string[];
+  };
+  answers: {
+    values: string[];
+    pictures: string[];
+  };
+  correctAnswer: string;
+  typeOfTask: string;
+}
+
+interface Task2 {
+  //тип даних для завдання співставлення
+  task: {
+    text: string;
+    table?: {
+      value1: string[];
+      velue2: string[];
+    };
+    image?: string;
+    list?: string[];
+  };
+  comparisonTable: {
+    list: {
+      list1: {
+        text?: string;
+        image?: string;
+      };
+      list2: {
+        text?: string;
+        image?: string;
+      };
+    };
+  };
+  сorrectComparison: {
+    [key: string]: string;
+  };
+  typeOfTask: string;
+}
+
+interface Task3 {
+  //тип даних для завдання з відкритою відповіддю
+  task: {
+    text: string;
+    table?: {
+      value1: string[];
+      velue2: string[];
+    };
+    image?: string;
+    list?: string[];
+  };
+  correctAnswer: string;
+  typeOfTask: string;
+}
+interface Tasks {
+  [key: string]: Task1 | Task2 | Task3; // Колекція з різними завданнями
+}
 interface Answers {
   values: string[];
   pictures: string[];
@@ -22,12 +88,15 @@ interface TaskData {
   typeOfTask: string;
   comparisonTable: Comparison;
 }
+
 interface TestItem {
   [key: string]: TaskData; // Колекція з різними завданнями
 }
 
 const Tests = () => {
   const [document, setDocument] = useState<TestItem>();
+
+  const [document1, setDocument1] = useState<Tasks>();
 
   const [userAnswers, setUserAnswers] = useState<{ [key: string]: any }>({});
 
@@ -45,9 +114,47 @@ const Tests = () => {
     }));
   };
 
-  console.log(userAnswers);
-
+  const isTask1 = (task: any): task is Task1 => task.typeOfTask === "choice";
+  const isTask2 = (task: any): task is Task2 =>
+    task.typeOfTask === "comparison";
+  const isTask3 = (task: any): task is Task3 =>
+    task.typeOfTask === "openAnswer";
   //ГЕНЕРУЄМО ТЕСТ
+
+  const getDocument1 = async () => {
+    try {
+      const docRef = doc(db, "topic 1", "variant 2"); // Створюємо посилання на документ
+      const docSnap = await getDoc(docRef); // Отримуємо дані документа
+
+      if (!docSnap.exists()) {
+        throw new Error("Документ не знайдено!");
+      }
+
+      const data = docSnap.data(); // data - об'єкт, де кожна властивість це завдання
+      const tasks: Tasks = Object.entries(data).reduce((acc, [key, value]) => {
+        if (isTask1(value)) {
+          acc[key] = value;
+        }
+        if (isTask2(value)) {
+          acc[key] = value;
+        }
+        if (isTask3(value)) {
+          acc[key] = value;
+        } else {
+          console.warn(`Невідомий тип завдання для ключа: ${key}`, value);
+        }
+        return acc;
+      }, {} as Tasks);
+      setDocument1(tasks);
+    } catch (error) {
+      console.error("Error getting document:", error);
+      throw error;
+    }
+  };
+  useEffect(() => {
+    getDocument1();
+  }, []);
+  console.log(document1);
 
   const getDocument = async () => {
     try {
@@ -55,20 +162,23 @@ const Tests = () => {
       const docSnap = await getDoc(docRef); // Отримуємо дані документа
 
       if (docSnap.exists()) {
-        const data = docSnap.data() as Partial<TestItem>;
+        // const data = docSnap.data() as Partial<TestItem>;
+        const data = docSnap.data();
 
         const finalData: TestItem = Object.entries(data).reduce(
           (acc, [key, taskData]) => {
-            acc[key] = {
-              task: taskData?.task ?? ["Немає завдання"],
-              answers: taskData?.answers ?? { values: [], pictures: [] },
-              correctAnswer: taskData?.correctAnswer ?? "Немає відповіді",
-              typeOfTask: taskData?.typeOfTask ?? "unknown",
-              comparisonTable: taskData?.comparisonTable ?? {
-                list1: [],
-                list2: [],
-              },
-            };
+            if (taskData.typeOfTask == "choice") {
+              acc[key] = {
+                task: taskData?.task ?? ["Немає завдання"],
+                answers: taskData?.answers ?? { values: [], pictures: [] },
+                correctAnswer: taskData?.correctAnswer ?? "Немає відповіді",
+                typeOfTask: taskData?.typeOfTask ?? "unknown",
+                comparisonTable: taskData?.comparisonTable ?? {
+                  list1: [],
+                  list2: [],
+                },
+              };
+            }
             addAnswer(key, "");
 
             return acc;
@@ -85,6 +195,7 @@ const Tests = () => {
       throw error;
     }
   };
+
   useEffect(() => {
     getDocument();
   }, []);
@@ -147,6 +258,20 @@ const Tests = () => {
   //ГЕНЕРУЄМО ТЕСТ
   return (
     <div>
+      {document1 &&
+        Object.entries(document1).map(([key, task]) => (
+          <div key={key}>
+            {isTask3(task) && (
+              <TaskOpenAnswer
+                task={task.task}
+                correctAnswer={task.correctAnswer}
+                typeOfTask={task.typeOfTask}
+                number={key}
+                func={EditUserAnswer}
+              />
+            )}
+          </div>
+        ))}
       <div className="tests">
         {document &&
           Object.entries(document).map(([key, task]) => (
